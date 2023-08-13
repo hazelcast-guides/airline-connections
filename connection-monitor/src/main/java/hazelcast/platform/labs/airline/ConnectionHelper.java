714dad25ff61b2d4fbdac16abecf8ce782c71e83
Randy May <randy.may@hazelcast.com>
2023-08-13 07:59:36 -0400

    updated ConnectionHelper to work with Windows and the new configuration format used by CLC 5.3.3

--- connection-monitor/src/main/java/hazelcast/platform/labs/airline/ConnectionHelper.java
+++ connection-monitor/src/main/java/hazelcast/platform/labs/airline/ConnectionHelper.java
@@ -1,14 +1,15 @@
 package hazelcast.platform.labs.airline;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.config.ClientConnectionStrategyConfig;
 import com.hazelcast.config.SSLConfig;
 import com.hazelcast.core.HazelcastInstance;
-import org.yaml.snakeyaml.Yaml;
 
-import java.io.*;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
 /**
@@ -16,13 +17,13 @@ import java.util.Properties;
  *
  * If a cluster name is passed to the connect method, it will attempt to access
  * cluster credentials inside the CLC vault on the local file system under the
- * ~/.hazelcast directory.  This method can be used to connect to Viridian and
- * non-Viridian clusters.
+ * ~/.hazelcast directory (or %HOMEDRIVE%:%HOMEPATH%\AppData\Roaming\Hazelcast on
+ * Windows).
  *
  * There is also a connect method that takes no arguments.  This method will first
  * inspect the VIRIDIAN_SECRETS_DIR environment variable and, if present, will
  * attempt to connect to a Viridian cluster using credentials from the environment
- * as described below.
+ * using the variables described below.
  *
  * VIRIDIAN_SECRETS_DIR  The unzipped contents of the file downloaded from the "Advanced" configuration tab
  * VIRIDIAN_CLUSTER_ID
@@ -102,9 +103,16 @@ public class ConnectionHelper {
         String secretsDir = System.getenv(VIRIDIAN_SECRETS_DIR_PROP);
         return secretsDir != null;
     }
+
     public static boolean configureViridianClientFromLocalVault(ClientConfig clientConfig, String clusterName){
         File homeDir = new File(System.getProperty("user.home"));
-        File hazelcastHomeDir = new File(homeDir, ".hazelcast");
+        String os = System.getProperty("os.name");
+        File hazelcastHomeDir;
+        if (os.toLowerCase().contains("windows")){
+            hazelcastHomeDir = new File(new File(new File(homeDir, "AppData"), "Roaming"), "Hazelcast");
+        } else {
+            hazelcastHomeDir = new File(homeDir, ".hazelcast");
+        }
         if (!hazelcastHomeDir.isDirectory())
             return false;  // RETURN
 
@@ -112,18 +120,16 @@ public class ConnectionHelper {
         if (!clusterConfigDir.isDirectory())
             return false; // RETURN
 
-        File configFile = new File(clusterConfigDir, "config.yaml");
+        File configFile = new File(clusterConfigDir, "config.json");
 
-        Yaml yaml = new Yaml();
-        try (InputStream inputStream = new FileInputStream(configFile)){
-            Map<String, Object> config = yaml.load(inputStream);
-            Map<String, String> clusterConfig = (Map<String, String>) config.get("cluster");
-            Map<String,String> sslConfig = (Map<String, String>) config.get("ssl");
-            String clusterId = clusterConfig.get("name");
-            String discoveryToken = clusterConfig.get("discovery-token");
-            String password = sslConfig.get("key-password");
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode root =  mapper.readTree(configFile);
+            String clusterId = root.get("cluster").get("name").asText();
+            String discoveryToken = root.get("cluster").get("discovery-token").asText();
+            String password = root.get("ssl").get("key-password").asText();
             configureViridian(clusterId, discoveryToken, password, clusterConfigDir.getAbsolutePath(), clientConfig);
-        } catch (IOException e) {
+        } catch(IOException x){
             // TODO add logging
             return false;
         }
